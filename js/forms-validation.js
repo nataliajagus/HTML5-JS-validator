7855ad2fda18d7a412136b7287d0150a979030a9
Natalia Jaguś <natalia.jagus@gmail.com>
2018-08-02 22:56:22 +0200

    Updated scripts and styles

--- js/forms-validation.js
+++ js/forms-validation.js
@@ -3,7 +3,83 @@
     function Validator(form) {
         this.form = form;
         this.fields = this.form.querySelectorAll("[required]");
+        this.errors = [];
+        this.errorsList = this.form.querySelector(".alert ol")
+
+        if(!this.fields.length) return;
+
+        this.form.onsubmit = function(e) {
+            e.preventDefault();
+
+            let formValid = this.validate();
+
+            if(formValid) {
+                this.form.submit();
+            } else {
+                return false;
+            }
+        }.bind(this);
     }
 
-})();
 
+    Validator.prototype.validate = function() {
+
+        this.clearErrors();
+
+        for(var i = 0; i < this.fields.length; i++) {
+            this.validateField(this.fields[i]);
+        }
+
+        if(!this.errors.length) {
+            return true;
+        } else {
+            this.showErrors();
+            return false;
+        }
+    };
+
+    Validator.prototype.validateField = function(field) {
+        let fieldValid =  field.validity.valid;
+
+        if(fieldValid) {
+            this.markAsValid(field);
+        } else {
+            this.errors.push(field.dataset.errorMessage);
+            this.markAsInvalid(field);
+        }
+    };
+
+    Validator.prototype.markAsValid = function(field) {
+      field.classList.remove("invalid");
+      field.classList.add("valid");
+    };
+
+    Validator.prototype.markAsInvalid = function(field) {
+        field.classList.remove("valid");
+        field.classList.add("invalid");
+    };
+
+    Validator.prototype.showErrors = function() {
+        let errorsListElements = document.createDocumentFragment();
+
+        for (let i = 0; i < this.errors.length; i++) {
+            let liEl = document.createElement("li");
+
+            liEl.textContent = this.errors[i];
+            errorsListElements.appendChild(liEl);
+
+        }
+
+        this.errorsList.appendChild(errorsListElements);
+        this.errorsList.parentNode.style.display = "block";
+    };
+
+    Validator.prototype.clearErrors = function() {
+        this.errors.length = 0;
+        this.errorsList.parentNode.style.display = "none";
+        this.errorsList.innerHTML = "";
+    };
+
+    let validator1 = new Validator(document.querySelector("#form"));
+
+})();
